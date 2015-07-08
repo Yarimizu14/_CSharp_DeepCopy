8b04a858de22224ed04f7789217a391b178fd03e
Kensei Yarimizu <yarimizu.kensei14@gmail.com>
2015-07-09 00:27:17 +0900

    中のクラスがコピーされていることを確認

---
+++ Assets/CopySample3.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class CopySample3
+{
+    public int field1;
+
+    private float _field2;
+    public float field2
+    {
+        get
+        {
+            return _field2;
+        }
+        set
+        {
+            _field2 = value;
+        }
+    }
+
+    public CopySample field3;
+
+    public override string ToString()
+    {
+        return "field1 : " + this.field1 + " field2 : " + _field2 + " field3 : " + this.field3.ToString();
+    }
+}
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,7 +6,8 @@ public class Test : MonoBehaviour
     // Use this for initialization
     void Start () {
 
-        this.ExecuteSample2();
+        //this.ExecuteSample2();
+        this.ExecuteSample3();
 
     }
 
@@ -41,7 +42,31 @@ public class Test : MonoBehaviour
 
         Debug.Log(sample);
         Debug.Log(copied);
+    }
 
+    void ExecuteSample3()
+    {
+        CopySample3 sample = new CopySample3();
+        sample.field1 = 10;
+        sample.field2 = 15f;
+        sample.field3 = new CopySample();
+        sample.field3.field1 = 123;
+
+        CopySample3 copied = sample.DeepCopy();
+
+        Debug.Log("Copied!!!!!");
+
+        Debug.Log(sample);
+        Debug.Log(copied);
+
+        copied.field1 = 100;
+        copied.field2 = 30f;
+        sample.field3.field1 = 321;
+
+        Debug.Log("Value Changed!!!!!");
+
+        Debug.Log(sample);
+        Debug.Log(copied);
     }
 
     // Update is called once per frame
